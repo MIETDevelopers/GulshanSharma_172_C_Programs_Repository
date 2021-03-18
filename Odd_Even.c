64e2ccab1a0f9bdfe41b2636cd91cf07e25a53b6
gs6699 <76106413+gs6699@users.noreply.github.com>
2021-03-18 18:52:20 +0530

    Odd_Even file

---
+++ Odd_Even.c
@@ -0,0 +1,15 @@
+//Author:Gulshan Sharma
+//Purpose: A C Program to check whether the entered number is Odd or Even.
+
+
+#include <stdio.h>
+int main() {           //Main Function Body
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);                     //It will scan the numbers entered by user.
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+    return 0;                               //Return Statement
+}
