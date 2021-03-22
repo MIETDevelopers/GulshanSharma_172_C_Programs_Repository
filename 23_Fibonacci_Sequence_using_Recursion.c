63f6e4cab39cccab489f2a8bbdb979c10acce3dc
gs6699 <76106413+gs6699@users.noreply.github.com>
2021-03-22 17:09:22 +0530

    Fibonacci sequence using recursion

---
+++ 23_Fibonacci_Sequence_using_Recursion.c
@@ -0,0 +1,23 @@
+//Author: Gulshan Sharma
+//Purpose:Fibonacci sequence using recursion
+
+#include<stdio.h> //Pre-Process directive to include input output header file.
+void printFibonacci(int n){   
+    static int n1=0,n2=1,n3;    
+    if(n>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         printf("%d ",n3);    
+         printFibonacci(n-1);    
+    }    
+}    
+int main(){    //main Function 
+    int n;    
+    printf("Enter the number of elements: ");    
+    scanf("%d",&n);    
+    printf("Fibonacci Series: ");    
+    printf("%d %d ",0,1);    
+    printFibonacci(n-2);//n-2 because 2 numbers are already printed    
+  return 0;         //Return Statement
+ }    
