607e90bd812d091557617ade769118ea5d64ac36
gs6699 <76106413+gs6699@users.noreply.github.com>
2021-03-30 20:23:32 +0530

    program to demonstarte do while loop

---
+++ 32_dowhileloop.c
@@ -0,0 +1,18 @@
+//Author: Gulshan Sharma
+//Purpose: To Demonstrate do while loop
+
+#include <stdio.h> //Pre-Process directive to include standard input output header file
+ 
+int main () { //Main function body starting
+
+   /* local variable definition */
+   int a = 10;
+
+   /* do loop execution */
+   do {
+      printf("value of a: %d\n", a);
+      a = a + 1;
+   }while( a < 15 );
+ 
+   return 0;    //return statement 
+}
