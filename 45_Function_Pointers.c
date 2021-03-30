9dee3dfcb20c023b632bb9c5e2867669fcfca50c
gs6699 <76106413+gs6699@users.noreply.github.com>
2021-03-30 21:32:36 +0530

    function Implimentation which return a structure

---
+++ 45_Function_Pointers.c
@@ -0,0 +1,27 @@
+//Author: Gulshan Sharma
+//Purpose: function implementation which returns a pointer.
+
+#include <stdio.h>//Pre Process directive to include standard input output functions header file
+
+// A normal function with an int parameter
+// and void return type
+void fun(int a)
+{
+    printf("Value of a is %d\n", a);
+}
+  
+int main()
+{
+    // fun_ptr is a pointer to function fun() 
+    void (*fun_ptr)(int) = &fun;
+  
+    /* The above line is equivalent of following two
+       void (*fun_ptr)(int);
+       fun_ptr = &fun; 
+    */
+  
+    // Invoking fun() using fun_ptr
+    (*fun_ptr)(10);
+  
+    return 0;   //return statemnet 
+}
