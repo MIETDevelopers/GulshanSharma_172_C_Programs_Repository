4d0225cf68cb0ee006a955bcb677e932b0376ff7
gs6699 <76106413+gs6699@users.noreply.github.com>
2021-03-30 23:25:28 +0530

    read csv file

--- 34_ReadCSV.c
+++ 34_ReadCSV.c
@@ -17,7 +17,7 @@ const char* getfield(char* line, int num){
     return NULL; 
 }
 int main(){			//main function body.
-    FILE* stream = fopen("C:\\Users\\HP\\Desktop\\C_Programs\\Aryan_133_C_Programs_Repository\\034_ReadCSV.csv", "r"); //Path of the .csv file.
+    FILE* stream = fopen("C:\\Users\\Gulshan\\Documents\\C_Programs\\GulshanSharma_172_C_Programs_Repository\\34_read_CSV.csv", "r"); //Path of the .csv file.
     char line[1024];        //Variable declarartion of character data type.
     while (fgets(line, 1024, stream)){		//while loop
         char* tmp = strdup(line);
